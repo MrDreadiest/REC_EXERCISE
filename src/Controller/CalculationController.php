8d4b255e13f3258c6b20a004bd1c10439f9e19cc
Tomasz Janiak <tomasz.janiak.1996@gmail.com>
2020-04-06 21:56:19 +0200

    calculate end send max occurrence.

--- src/Controller/CalculationController.php
+++ src/Controller/CalculationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,12 +19,30 @@ class CalculationController extends AbstractController
     }
 
     /**
-     * @Route("/calculation", name="calculation")
+     * @Route("/calculation/{n}", name="calculation")
      */
-    public function calculation()
+    public function calculation($n)
     {
+        /** @var array $outputArray */
+        $outputArray = new ArrayCollection([0, 1]);
+        $maxOccurrence = PHP_INT_MIN;
+
+        if (2 > $n) $maxOccurrence = $outputArray[$n];
+        else {
+            for ($i = 2; $i <= $n; $i++) {
+
+                if (0 == $i % 2) {
+                    $outputArray->add($outputArray[$i / 2]);
+                }
+                elseif (1 == $i % 2) {
+                    $outputArray->add($outputArray[$i / 2] + $outputArray[($i / 2) + 1]);
+                }
+                if ($outputArray[$i] > $maxOccurrence) $maxOccurrence = $outputArray[$i];
+            }
+        }
+
         return $this->render('calculation/index.html.twig', [
-            'controller_name' => 'CalculationController',
+            'maxOccurrence' => $maxOccurrence,
         ]);
     }
 }
