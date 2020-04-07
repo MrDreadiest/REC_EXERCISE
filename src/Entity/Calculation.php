69c8a0da8d583f023674e8caf0eca068ed87eb69
Tomasz Janiak <tomasz.janiak.1996@gmail.com>
2020-04-07 10:53:29 +0200

    Visual changes.

--- src/Entity/Calculation.php
+++ src/Entity/Calculation.php
@@ -4,8 +4,17 @@ namespace App\Entity;
 
 class Calculation
 {
+    /**
+     * @var
+     */
     private $n;
+    /**
+     * @var int
+     */
     private $maxOccurrence;
+    /**
+     * @var array
+     */
     private $series;
 
     /**
@@ -16,44 +25,12 @@ class Calculation
     {
         $this->n = $n;
         $this->maxOccurrence = PHP_INT_MIN;
-        $this->series = [0,1];
+        $this->series = [0, 1];
     }
 
-    public function getN(): ?int
-    {
-        return $this->n;
-    }
-
-    public function setN(int $n): void
-    {
-        $this->n = $n;
-    }
-
-    public function getMaxOccurrence(): ?int
-    {
-        return $this->maxOccurrence;
-    }
-
-    public function setMaxOccurrence(?int $maxOccurrence): void
-    {
-        $this->maxOccurrence = $maxOccurrence;
-    }
-
-    public function getSeries(): ?array
-    {
-        return $this->series;
-    }
-
-    public function setSeries(?array $series): void
-    {
-        $this->series = $series;
-    }
-
-    public function addToSeries(int $value): void
-    {
-        $this->series[] = $value;
-    }
-
+    /**
+     * Calculate largest number in a series
+     */
     public function calculate(): void
     {
         if (2 > $this->getN()) $this->setMaxOccurrence($this->getSeries()[$this->getN()]);
@@ -62,12 +39,67 @@ class Calculation
 
                 if (0 == $i % 2) {
                     $this->addToSeries($this->getSeries()[$i / 2]);
-                }
-                elseif (1 == $i % 2) {
+                } elseif (1 == $i % 2) {
                     $this->addToSeries($this->getSeries()[$i / 2] + $this->getSeries()[($i / 2) + 1]);
                 }
                 if ($this->getSeries()[$i] > $this->getMaxOccurrence()) $this->setMaxOccurrence($this->getSeries()[$i]);
             }
         }
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getN(): ?int
+    {
+        return $this->n;
+    }
+
+    /**
+     * @param int $n
+     */
+    public function setN(int $n): void
+    {
+        $this->n = $n;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getSeries(): ?array
+    {
+        return $this->series;
+    }
+
+    /**
+     * @param array|null $series
+     */
+    public function setSeries(?array $series): void
+    {
+        $this->series = $series;
+    }
+
+    /**
+     * @param int $value
+     */
+    public function addToSeries(int $value): void
+    {
+        $this->series[] = $value;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getMaxOccurrence(): ?int
+    {
+        return $this->maxOccurrence;
+    }
+
+    /**
+     * @param int|null $maxOccurrence
+     */
+    public function setMaxOccurrence(?int $maxOccurrence): void
+    {
+        $this->maxOccurrence = $maxOccurrence;
     }
 }
